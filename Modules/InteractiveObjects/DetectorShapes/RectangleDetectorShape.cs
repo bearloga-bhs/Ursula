6d2b03f14630a3fae6c29d30b128a7cbd4a1ebba
@artemsap <a.sapozhnikov00@gmail.com>
2025-11-08 22:57:45 +0300

    make base for rectangle detector

--- Modules/InteractiveObjects/DetectorShapes/RectangleDetectorShape.cs
+++ Modules/InteractiveObjects/DetectorShapes/RectangleDetectorShape.cs
@@ -1,30 +1,52 @@
-﻿using Godot;
+using System;
+using Godot;
 
 public class RectangleDetectorShape : IDetectorShape
 {
-    private Vector2 min;
-    private Vector2 max;
-    
+    private Vector3 left_down;
+    private Vector3 left_up;
+    private Vector3 right_down;
+    private Vector3 right_up;
+    private Vector3 offset;
+
     private Node3D anchor;
     
-    public RectangleDetectorShape(Vector2 min, Vector2 max, Node3D relativeTo)
+    public RectangleDetectorShape(Node3D relativeTo, float width, float height, Vector3 offset)
     {
-        this.min = min;
-        this.max = max;
+        //TODO add support of offset of rectangle points
+        left_down = new Vector3(-width / 2, 0, -height / 2);
+        left_up = new Vector3(-width / 2, 0, height / 2);
+        right_down = new Vector3(width / 2, 0, -height / 2);
+        right_up = new Vector3(width / 2, 0, height / 2);
+        this.offset = offset;
         anchor = relativeTo;
     }
 
     public bool IsDetected(Vector3 point)
     {
-        Vector3 offset = anchor.GlobalPosition;
+        //TODO rotate rectangle here, ignore Pitch rotation
+        Vector3 point_copy = new Vector3(point.X, 0, point.Z);
+        
+        Vector3 left_side = left_up - left_down;
+        Vector3 up_side = right_up - left_up;
+        Vector3 right_side = right_down - right_up;
+        Vector3 down_side = left_down - right_down;
+        
+        Vector3 point_left = left_down - point_copy;
+        Vector3 point_up = left_up - point_copy;
+        Vector3 point_right = right_up - point_copy;
+        Vector3 point_down = right_down - point_copy;
 
-        if (point.X >= min.X + offset.X &&
-            point.X <= max.X + offset.X &&
-            point.Z >= min.Y + offset.Z &&
-            point.Z <= max.Y + offset.Z)
+        float cross_left = point_left.Cross(left_side).Y;
+        float cross_up = point_up.Cross(up_side).Y;
+        float cross_right = point_right.Cross(right_side).Y;
+        float cross_down = point_down.Cross(down_side).Y;
+
+        if (cross_left < 0 || cross_up < 0 || cross_right < 0 || cross_down < 0)
         {
-            return true;
+            return false;
         }
-        return false;
+        
+        return true;
     }
 }
